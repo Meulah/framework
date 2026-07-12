98a6a58941d2350ba48c21b4feb1768a3d5ec6c6
amospikins <sanmco2013@gmaile.com>
2026-07-12 21:23:44 +0100

    Move application starter to Meulah/meulah repository

--- skeleton/config/http.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Meulah\Support\Environment;
-
-return [
-    'max_body_size' => (int) Environment::get('HTTP_MAX_BODY_SIZE', 10_485_760),
-];
--- skeleton/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use App\Controllers\HomeController;
-use Meulah\Routing\Router;
-
-/** @var Router $router */
-$router->get('/', HomeController::class, 'home');
